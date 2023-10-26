793958aa4d629f2f542f76549945dd98ff7ad53e
CBTech1 <101056464+CBTech1@users.noreply.github.com>
2023-10-26 02:51:57 +0000

    Added chakra ui/react feature called fitted tabs

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 // import what we need for home
-import { Container } from "@chakra-ui/react";
+import { Container, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import Auth from "../components/Auth";
 import TodoList from "../components/TodoList";
 
@@ -7,7 +7,26 @@ export default function Home() {
   return (
     <Container maxW="7xl">
       <Auth />
-      <TodoList />
+      <Tabs isFitted variant='enclosed'>
+        <TabList mb='1em'>
+          <Tab>Todos</Tab>
+          <Tab>Events</Tab>
+          <Tab>Contacts</Tab>
+        </TabList>
+        
+        <TabPanels>
+          <TabPanel>
+             <p>TodoList!</p>
+             <TodoList />
+          </TabPanel>
+          <TabPanel>
+            <p>EventList!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>ContactList!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </Container>
   )
 };
