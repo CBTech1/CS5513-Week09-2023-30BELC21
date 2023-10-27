2ab3c950ea5853a9c3adebb69cc2365d075e6a1f
CBTech1 <101056464+CBTech1@users.noreply.github.com>
2023-10-27 03:29:53 +0000

    changed " to ' in my index.js

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 import { Container, Box, Link, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
-import Auth from "../components/Auth";
-import TodoList from "../components/TodoList";
-import EventsList from "../components/EventsList";
-import AddEvents from "../components/AddEvents";
-import AddTodo from "../components/AddTodo";
-import AddContacts from "../components/AddContacts";
-import ContactList from "../components/ContactList";
+import Auth from '../components/Auth';
+import TodoList from '../components/TodoList';
+import EventsList from '../components/EventsList';
+import AddEvents from '../components/AddEvents';
+import AddTodo from '../components/AddTodo';
+import AddContacts from '../components/AddContacts';
+import ContactList from '../components/ContactList';
 
 export default function Home() {
   return (
-    <Container maxW="7xl">
+    <Container maxW='7xl'>
       <Auth />
       <Tabs isFitted variant='enclosed'>
         <TabList mb='1em'>
